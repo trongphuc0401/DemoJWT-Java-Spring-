4e55fb6251856c34836dedb389738c8aa3277337
Nguyen Trong Phuc <93772238+trongphuc0401@users.noreply.github.com>
2024-09-04 14:11:48 +0700

    update

---
+++ src/main/java/vn/edu/likelion/DemoAuthJWT/DemoAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.likelion.DemoAuthJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthJwtApplication.class, args);
+	}
+
+}
